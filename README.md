00e1183f78f738367c09c8e54a437b7211cc4f2e
shubh045 <shubh6441@gmail.com>
2022-11-01 15:58:23 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio Website
+
+This is a portfolio website developed using Reactjs
---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Landing from "./components/Layout/Landing";
+import Navbar from "./components/Layout/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Landing />
+    </>
+  );
+};
+
+export default App;
