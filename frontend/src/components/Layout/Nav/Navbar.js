23b004ecff99dc6d2d727d8f2521c277bbe1efeb
shubh045 <shubh6441@gmail.com>
2022-11-06 04:03:01 +0530

    style: Create active class for nav item

--- frontend/src/components/Layout/Nav/Navbar.js
+++ frontend/src/components/Layout/Nav/Navbar.js
@@ -1,24 +1,63 @@
-import {AiOutlineHome} from 'react-icons/ai';
-import {AiOutlineUser} from 'react-icons/ai';
-import {AiOutlineProject} from 'react-icons/ai';
-import {AiOutlineContacts} from 'react-icons/ai';
-import {AiOutlineExperiment} from 'react-icons/ai';
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { AiOutlineProject } from "react-icons/ai";
+import { AiOutlineContacts } from "react-icons/ai";
+import { AiOutlineExperiment } from "react-icons/ai";
 import { BiBook } from "react-icons/bi";
-import './navbar.css';
+import { useState } from "react";
+import "./navbar.css";
 
 const Navbar = () => {
+  const [activeNav, setActiveNav] = useState("#");
+
   return (
     <>
       <nav>
-        <a href="#"><AiOutlineHome /></a>
-        <a href="#about"><AiOutlineUser /></a>
-        <a href="#skills"><BiBook /></a>
-        <a href="#experience"><AiOutlineExperiment /></a>
-        <a href="#projects"><AiOutlineProject /></a>
-        <a href="#contact"><AiOutlineContacts /></a>
+        <a
+          href="#"
+          onClick={() => setActiveNav("#")}
+          className={activeNav === "#" ? "active" : ""}
+        >
+          <AiOutlineHome />
+        </a>
+        <a
+          href="#about"
+          onClick={() => setActiveNav("#about")}
+          className={activeNav === "#about" ? "active" : ""}
+        >
+          <AiOutlineUser />
+        </a>
+        <a
+          href="#skills"
+          onClick={() => setActiveNav("#skills")}
+          className={activeNav === "#skills" ? "active" : ""}
+        >
+          <BiBook />
+        </a>
+        <a
+          href="#experience"
+          onClick={() => setActiveNav("#experience")}
+          className={activeNav === "#experience" ? "active" : ""}
+        >
+          <AiOutlineExperiment />
+        </a>
+        <a
+          href="#projects"
+          onClick={() => setActiveNav("#projects")}
+          className={activeNav === "#projects" ? "active" : ""}
+        >
+          <AiOutlineProject />
+        </a>
+        <a
+          href="#contact"
+          onClick={() => setActiveNav("#contact")}
+          className={activeNav === "#contact" ? "active" : ""}
+        >
+          <AiOutlineContacts />
+        </a>
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
